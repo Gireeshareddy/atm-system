2bb869a1a714ad2e738533d72a264a4d279648af
Gireesha Reddy <69247824+Gireeshareddy@users.noreply.github.com>
2021-06-13 15:55:01 +0530

    Add files via upload

---
+++ atm.py
@@ -0,0 +1,48 @@
+
+import time
+
+print("Please insert your CARD")
+
+time.sleep(5)
+
+password = 1234
+
+pin = int(input("enter your atm pin "))
+
+balance = 5000
+
+if pin==password:
+  while True:
+    print("""
+        1 == balance
+        2 == withdraw balance
+        3 == deposit balance
+        4 == exit
+        """
+        )
+    try:
+      option=int(input("Please enter your choice "))
+    except:
+      print("Please enter a valid option")
+
+    if option==1:
+      print(f"your current balance is {balance}")
+  
+    if option==2:
+
+      withdraw_amount=int(input("please enter withdraw_amount "))
+      balance = balance - withdraw_amount
+      print(f"{withdraw_amount} is debited from your account")
+      print(f"your updated balance is {balance}")
+  
+    if option ==3:
+
+      deposit_amount=int(input("please enter deposit_amount"))
+      balance = balance + deposit_amount
+      print(f"{deposit_amount} is credited to your account")
+      print(f"your updated balance is {balance}")
+
+    if option==4:
+      break
+else:
+  print("wrong pin please try again")
